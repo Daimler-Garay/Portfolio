3214e8ac61c6c08f92ba90e458e0bc1a5c08cd10
H41SH4 <daimlerchryslergaray@gmail.com>
2023-10-30 18:17:56 -0500

    Updated

--- script/script.js
+++ script/script.js
@@ -2,17 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector('.navbar');
 
     if (header) {
-        // Element with class 'navbar' was found
         window.onscroll = function() {
             var top = window.scrollY;
-            if (top >= 100) {
+            if (top >= 0) {
                 header.classList.add('navbarDark');
             } else {
                 header.classList.remove('navbarDark');
             }
         }
     } else {
-        // Element with class 'navbar' was not found
         console.log("No element with class 'navbar' found in your HTML.");
     }
 });
